159552f9a12080ee2f64e111fa9ae5994ee5255e
haricharanpakanatii <haricharanpakanatii@gmail.com>
2026-02-04 17:12:03 +0530

    Feature: Added Loan Duration Categories
    Implemented CASE WHEN logic to group loans into Short, Middle, and Long term buckets.

--- german-credit-risk/risk_analysis.sql..sql
+++ german-credit-risk/risk_analysis.sql..sql
@@ -1,14 +1,11 @@
--- MISSION 10 & 11: Get the list of High-Risk Renters (Sorted by Highest Debt)
-SELECT Age, Sex, Housing, "Credit amount", "Credit Risk"
+SELECT Duration,
+CASE
+WHEN Duration < 12 THEN "Short Term"
+WHEN Duration > 24 THEN "Long Term"
+ ELSE "Middle Term"
+END AS "Duration_category",
+COUNT(*) AS "Total_loans"
 FROM loans_clean
-WHERE Housing = 'rent'
-  AND Age < 30
-  AND "Credit Risk" = 2
-ORDER BY "Credit amount" DESC;
+GROUP BY "Duration_category";
 
--- MISSION 12: Calculate the Total Financial Exposure (The $$$ Amount)
-SELECT SUM("Credit amount") AS "Total_Risk_Value"
-FROM loans_clean
-WHERE Housing = 'rent'
-  AND Age < 30
-  AND "Credit Risk" = 2;
+
