8375b453ee58d300d8fd54ce51e30b2b1412e495
haricharanpakanatii <haricharanpakanatii@gmail.com>
2026-02-05 10:59:40 +0530

    Feature: Filtered for Popular Loan Categories.

--- german-credit-risk/risk_analysis.sql..sql
+++ german-credit-risk/risk_analysis.sql..sql
@@ -1,3 +1,4 @@
+-- MISSION 14: Find "Popular" Loan Categories (More than 300 loans)
 SELECT Duration,
 CASE
 WHEN Duration < 12 THEN "Short Term"
@@ -6,6 +7,7 @@ WHEN Duration > 24 THEN "Long Term"
 END AS "Duration_category",
 COUNT(*) AS "Total_loans"
 FROM loans_clean
-GROUP BY "Duration_category";
+GROUP BY "Duration_category"
+HAVING Total_loans > 300;
 
 
